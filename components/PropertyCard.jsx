ed5be20b6c6b1f6dc8d8e053fcfece031bd9a4d6
Mongezi <momasombuka@gmail.com>
2025-07-30 00:57:57 +0300

    change currency

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -12,11 +12,11 @@ const PropertyCard = ({ property }) => {
   const getRateDisplay = () => {
     const { rates } = property;
     if (rates.monthly) {
-      return `$${rates.monthly.toLocaleString()}/mo`;
+      return `R${rates.monthly.toLocaleString()}/mo`;
     } else if (rates.weekly) {
-      return `$${rates.weekly.toLocaleString()}/wk`;
+      return `R${rates.weekly.toLocaleString()}/wk`;
     } else if (rates.nightly) {
-      return `$${rates.nightly.toLocaleString()}/night`;
+      return `R${rates.nightly.toLocaleString()}/night`;
     }
   };
 
